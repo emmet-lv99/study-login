801116e4a70420d3bd2cd181fa64a2120470b62e
dachanisfull@gmail.com <dachan.kim>
2021-08-14 23:44:54 +0900

    로그인 인풋 추가

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,67 @@
 <template>
   <v-container>
-    123
+<!--
+-->
+    <v-card
+      max-width="600"
+    >
+      <form>
+        <v-app-bar
+          flat
+        ></v-app-bar>
+        <v-text-field
+            v-model="name"
+            :rules="rules.name"
+            label="First name"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Normal with hint text"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+        ></v-text-field>
+        <v-btn
+            class="mr-4"
+            @click="submit"
+        >
+          submit
+        </v-btn>
+        <v-btn @click="clear">
+          clear
+        </v-btn>
+      </form>
+    </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "Login.vue"
+
+  name: "Login.vue",
+  data: () => ({
+    name: '',
+    password: '',
+    show1: false,
+    rules: {
+      required: value => !!value || 'Required.',
+      min: v => v.length >= 8 || 'Min 8 characters',
+      name: [val => (val || '').length > 0 || 'This field is required'],
+    },
+  }),
+  methods: {
+    submit () {
+      console.log(123)
+    },
+    clear () {
+      console.log(456)
+    },
+  }
 }
 </script>
 
